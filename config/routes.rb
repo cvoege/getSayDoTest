f9f05e72c798203a2db296b2d1849ecbd4eac9c6
Colton <cvoege@gmail.com>
2014-05-26 16:35:51 -0400

    Finish layot and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+  get 'users/new'
 
-  get 'static_pages/home'
+  match '/about', to: 'static_pages#about', via: 'get'
 
-  get 'static_pages/help'
+  match '/help', to: 'static_pages#help', via: 'get'
+
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
+  match '/signup', to: 'users#new', via: 'get'
+
+  root 'static_pages#home'
 
   resources :microposts
 
